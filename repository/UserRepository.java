1b03d75221c089920ad6b15b6eb9951754b33982
elvis.goncalves <elvisrgoncalves@hotmail.com>
2022-01-28 10:15:14 -0300

    primeiro commit

---
+++ repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.dev.aulajparepository.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.dev.aulajparepository.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+	@Query("SELECT obj FROM User obj WHERE obj.salary >= :minSalary AND obj.salary <= :maxSalary")
+	Page<User> searchBySalary(Double minSalary, Double maxSalary, Pageable pageable);
+
+	@Query("SELECT obj FROM User obj WHERE LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%'))")
+	Page<User> searchByName(String name, Pageable pageable);
+
+	Page<User> findBySalaryBetween(Double minSalary, Double maxSalary, Pageable pageable);
+	
+	Page<User> findByNameContainingIgnoreCase(String name, Pageable pageable);
+	
+}
